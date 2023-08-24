980018e7cd579967b0f953a49043791b49d54769
Jagannathan Moganaraj <mjagannathan@athenahealth.com>
2023-08-24 17:57:15 +0530

    commitng affinity & adding helper scripts

--- helper.sh
+++ helper.sh
@@ -10,7 +10,9 @@ then
 fi
 echo $HELM_NAME
 
-# helm template ./argocd-poc-helm-guestbook --namespace=${NAMESPACE} --set global.k8sEnv=$ENV --set branchIdentifier=$BRANCH_ID
+helm template ./argocd-poc-helm-guestbook \
+  --namespace=${NAMESPACE} --set global.k8sEnv=$ENV \
+  --set branchIdentifier=$BRANCH_ID
 
 helm upgrade --install $HELM_NAME ./argocd-poc-helm-guestbook \
   --namespace=${NAMESPACE} \
@@ -39,19 +41,27 @@ mkdir argocd-poc-prod2-platform
 
 
 
-ENV=poc-prod1
+ENV=poc-dev1
 NAMESPACE=argocd-${ENV}-platform
 
-BRANCH_ID=""
+BRANCH_ID="zxc001"
 
 HELM_NAME=${NAMESPACE}-macroservice
+
+MANIFEST_DIR=namespaces/${NAMESPACE}/macroservice
 if [ $BRANCH_ID ]
 then
-  HELM_NAME=${HELM_NAME}-${BRANCH_ID}
+  MANIFEST_DIR=${MANIFEST_DIR}-${BRANCH_ID}
 fi
-echo $HELM_NAME
+echo $MANIFEST_DIR
 
+mkdir $MANIFEST_DIR
 helm template ./argocd-poc-helm-guestbook \
   --namespace=${NAMESPACE} --set global.k8sEnv=$ENV \
   --set branchIdentifier=$BRANCH_ID \
-  > argocd-poc-gitops/namespaces/${NAMESPACE}/$HELM_NAME.yaml
+  > argocd-poc-gitops/${MANIFEST_DIR}/manifest.yaml
+
+argocd app create $HELM_NAME \
+  --repo https://github.com/jagannathan-m/argocd-poc-gitops.git \
+  --path ${MANIFEST_DIR} \
+  --dest-server https://kubernetes.default.svc --dest-namespace ${NAMESPACE}
